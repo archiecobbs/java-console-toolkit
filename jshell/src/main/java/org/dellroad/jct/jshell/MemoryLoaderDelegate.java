6ce2ce549f23c57a83fc99dd9a8c75b0f360f5f6
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-01-03 18:08:48 -0600

    Add support for jar: URLs in MemoryLoaderDelegate.addToClasspath().

--- jshell/src/main/java/org/dellroad/jct/jshell/MemoryLoaderDelegate.java
+++ jshell/src/main/java/org/dellroad/jct/jshell/MemoryLoaderDelegate.java
@@ -6,8 +6,11 @@
 package org.dellroad.jct.jshell;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import jdk.jshell.execution.LoaderDelegate;
 import jdk.jshell.spi.ExecutionControl.ClassBytecodes;
@@ -19,8 +22,16 @@ import org.dellroad.stuff.java.MemoryClassLoader;
 
 /**
  * A JShell {@link LoaderDelegate} that uses a {@link MemoryClassLoader}.
+ *
+ * <p>
+ * This class accepts {@code --class-path} components that refer to JAR file contents using the {@code jar:} URL syntax,
+ * e.g., {@code jar:file:/some/dir/foo.jar!/BOOT-INF/lib/bar.jar}. However, this is only partially useful because
+ * (a) you can't build a classpath with components that contain colon characters, and (b) the jshell tool rejects
+ * classpath components that aren't real files.
  */
 public class MemoryLoaderDelegate implements LoaderDelegate {
+
+    private static final String JAR_FILE_PREFIX = "jar:file:";
 
     private final HashMap<String, Class<?>> classMap = new HashMap<>();
     private final MemoryClassLoader loader;
@@ -65,10 +76,10 @@ public class MemoryLoaderDelegate implements LoaderDelegate {
         if (path == null)
             throw new IllegalArgumentException("null path");
         try {
-            for (String elem : path.split(File.pathSeparator)) {
-                if (elem.isEmpty())
-                    continue;
-                this.loader.addURL(new File(elem).toURI().toURL());
+            for (String component : this.splitClassPath(path)) {
+                this.loader.addURL(component.startsWith(JAR_FILE_PREFIX) ?
+                  new URL(component) :
+                  new File(component).toURI().toURL());
             }
         } catch (Exception e) {
             throw this.wrapCause(e, new InternalException(e.getMessage()));
@@ -119,5 +130,21 @@ public class MemoryLoaderDelegate implements LoaderDelegate {
     private <T extends Throwable, T2 extends Throwable> T2 wrapCause(T t, T2 t2) {
         t2.initCause(t);
         return t2;
+    }
+
+    private List<String> splitClassPath(String path) {
+        final ArrayList<String> components = new ArrayList<>();
+        for (int start = 0; start < path.length(); ) {
+            int look = start;
+            if (path.substring(start).startsWith(JAR_FILE_PREFIX))
+                look += JAR_FILE_PREFIX.length();
+            final int sep = path.indexOf(File.pathSeparatorChar, look);
+            final int stop = sep != -1 ? sep : path.length();
+            final String component = path.substring(start, stop);
+            if (!component.isEmpty())
+                components.add(component);
+            start = Math.max(stop, sep + 1);
+        }
+        return components;
     }
 }
