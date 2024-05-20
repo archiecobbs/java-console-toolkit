f8649fdb8190e5529a48ebecb90988b3cf094a6d
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-05-20 15:36:46 -0500

    Regenerate Maven site after release 1.0.4.

--- site/testapidocs/type-search-index.js
+++ site/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.dellroad.jct.core.simple","l":"SimpleCommandLineParserTest"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.dellroad.jct.core.simple","l":"SimpleCommandLineParserTest"}]
