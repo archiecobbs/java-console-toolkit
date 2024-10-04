9d2d7a70c12f1238829bd23820042bac119caa3b
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-10-04 13:39:01 -0500

    Regenerate Maven site after release 1.0.5.

--- site/testapidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.dellroad.jct.core.simple"}]
--- site/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.dellroad.jct.core.simple","l":"SimpleCommandLineParserTest"}]
